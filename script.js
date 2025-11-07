d066a15af33c864489505f473de6a5697a2fda90
Mohsen Zamani <mohsenzamanist@gmail.com>
2025-11-07 21:41:59 +0000

    Full functional

--- script.js
+++ script.js
@@ -29,19 +29,13 @@ window.addEventListener("load", () => {
   fetchAndRender(defaultCity);
 });
 
-async function fetchAndRender(city) {
-  const APIKey = "f404cb64a0a4fcd6b8867678cd73a93e";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`;
-  const data = await getWeather(url);
-  console.log(data);
-  render(data);
-}
-
 cityInput.addEventListener("keyup", () => {
   if (/^[A-Za-z\s'-]+$/.test(cityInput.value)) {
     errorP.style.visibility = "hidden";
     submitBtn.disabled = false;
   } else {
+    errorP.textContent =
+      "Only alphabet characters (a-z, A-Z), spaces, hyphens, and apostrophes.";
     errorP.style.visibility = "visible";
     submitBtn.disabled = true;
     submitBtn.style.borderColor = "black";
@@ -55,6 +49,44 @@ weatherForm.addEventListener("submit", async (event) => {
   fetchAndRender(city);
 });
 
+function windDirection(deg) {
+  const directions = [
+    "N",
+    "NNE",
+    "NE",
+    "ENE",
+    "E",
+    "ESE",
+    "SE",
+    "SSE",
+    "S",
+    "SSW",
+    "SW",
+    "WSW",
+    "W",
+    "WNW",
+    "NW",
+    "NNW",
+  ];
+  const index = Math.round(deg / 22.5) % 16;
+  return directions[index];
+}
+
+async function fetchAndRender(city) {
+  const APIKey = "f404cb64a0a4fcd6b8867678cd73a93e";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`;
+  const data = await getWeather(url);
+  if (data.cod !== 200) {
+    const fallBackUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${APIKey}&units=metric`;
+    const fallBackData = await getWeather(fallBackUrl);
+    errorP.textContent = "City with given name could not be found.";
+    errorP.style.visibility = "visible";
+    render(fallBackData);
+  } else {
+    render(data);
+    cityInput.value = "";
+  }
+}
 async function getWeather(url) {
   try {
     return await (await fetch(url)).json();
@@ -73,15 +105,20 @@ function render(data) {
     month: "long",
     day: "numeric",
   })}`;
-  tempDegree.textContent = data.main.temp;
-  desc.textContent = data.weather[0].description;
-  pressure.textContent = data.main.pressure;
-  windSpeed.textContent = data.wind.speed;
-  humidity.textContent = data.main.humidity;
-  windDegreeSpan.textContent = data.wind.deg;
-  feelsLikeSpan.textContent = data.main.feels_like;
-  maxTempSpan.textContent = data.main.temp_max;
-  minTempSpan.textContent = data.main.temp_min;
+  tempDegree.textContent = data.main.temp + "°C";
+  desc.textContent = data.weather[0].description
+    .split(" ")
+    .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+    .join(" ");
+  pressure.textContent = data.main.pressure + "hpa";
+  windSpeed.textContent = data.wind.speed + "km/h";
+  humidity.textContent = data.main.humidity + "%";
+  windDegreeSpan.textContent = `${data.wind.deg} - ${windDirection(
+    data.wind.deg
+  )}`;
+  feelsLikeSpan.textContent = data.main.feels_like + "°C";
+  maxTempSpan.textContent = data.main.temp_max + "°C";
+  minTempSpan.textContent = data.main.temp_min + "°C";
   const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
   icon.src = iconUrl;
 }
